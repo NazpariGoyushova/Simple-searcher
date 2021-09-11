c923e5a704f1b1b6a5c7b2464e27923846979171
Nazpari Goyushova <88227525+NazpariGoyushova@users.noreply.github.com>
2021-09-11 18:46:34 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var search=document.getElementById("search");
+var text=document.getElementById("text");
+var micro=document.getElementById("micro");
+var box=document.getElementById("bx");
+
+function img(){
+bx.classList.toggle("active");
+
+}
